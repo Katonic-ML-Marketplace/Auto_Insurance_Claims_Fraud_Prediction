d4ff018e27ba3bd4eda18f68ead781a9788e1eb9
shreya01807 <disha.uke@katonic.ai>
2022-11-04 12:28:47 +0000

    added files

---
+++ build.sh
@@ -0,0 +1,3 @@
+docker build -t katonic/apps:auto-insurance-claims-fraud-prediction .
+docker push katonic/apps:auto-insurance-claims-fraud-prediction
+# docker run --rm -p 8050:8050 katonic/apps:auto-insurance-claims-fraud-prediction
